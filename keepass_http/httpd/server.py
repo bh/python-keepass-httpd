dfec77e016ab5fb70fe04175f2bcf862b167d622
Benjamin Hedrich <kiwisauce@pagenotfound.de>
2014-02-23 16:42:47 +0100

    Add the HTTP server which receives requests from ChromeIPass
    Also include a runnabled script to run this server from commandline.

---
+++ keepass_http/httpd/server.py
@@ -0,0 +1,67 @@
+import json
+import SocketServer
+
+from keepass_http.httpd import requests
+from keepass_http.utils import logging
+
+log = logging.getLogger(__name__)
+
+
+class KeepassHTTPServer(SocketServer.ThreadingTCPServer):
+    allow_reuse_address = True
+
+    def __init__(self, *args, **kwargs):
+        SocketServer.ThreadingTCPServer.__init__(self, *args, **kwargs)
+        self._is_daemon = None
+        self.backend = None
+
+    def set_backend(self, backend):
+        self.backend = backend
+
+    def set_is_daemon(self, is_daemon):
+        self._is_daemon = is_daemon
+
+    def handle_error(self, request, client_address):
+        """
+        """
+        client_host, client_port = client_address
+        log.exception("An error has been occured for client: %s:%s" % (client_host,
+                                                                       client_port))
+
+
+class KeepassHTTPRequestHandler(SocketServer.BaseRequestHandler):
+
+    def _extract_request_body(self):
+        http_request = self.request.recv(1024).strip()
+        unused_http_header, body = http_request.split('\r\n\r\n')
+        return json.loads(body)
+
+    def _write_reponse(self, response):
+        self.request.sendall(json.dumps(response))
+
+    def handle(self):
+        request_body = self._extract_request_body()
+        response_content = self.handle_request(request_body)
+        self._write_reponse(response_content)
+
+    def handle_request(self, request_dict):
+        request_type = request_dict["RequestType"]
+
+        if request_type == "test-associate":
+            request = requests.TestAssociateRequest(self.server)
+
+        elif request_type == "associate":
+            request = requests.AssociateRequest(self.server)
+
+        elif request_type == "get-logins":
+            request = requests.GetLoginsRequest(self.server)
+
+        elif request_type == "set-login":
+            request = requests.SetLoginRequest(self.server)
+
+        else:
+            raise NotImplementedError(
+                "Request type %s is not yet implemented" %
+                request_dict["RequestType"])
+
+        return request.get_response(request_dict)
---
+++ scripts/python-keepass-httpd.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+"""
+Usage:
+  ./python-keepass-httpd.py start <database_path> <passphrase> [options]
+  ./python-keepass-httpd.py (-h | --help)
+  ./python-keepass-httpd.py --version
+
+Options:
+  --help                  Show this screen.
+  -v --version            Show version.
+  -d --daemon             Start as daemon
+  -p --port PORT          Specify a port [default: 19455]
+  -h --host HOST          Specify a host [default: 127.0.0.1]
+
+"""
+
+import sys
+
+import daemon
+
+import docopt
+import lockfile
+import setproctitle
+
+from keepass_http import backends
+from keepass_http.httpd.server import (KeepassHTTPRequestHandler,
+                                       KeepassHTTPServer)
+from keepass_http.utils import logging
+
+sys.path.append("..")
+
+
+log = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    arguments = docopt.docopt(__doc__)
+
+    is_daemon = arguments["--daemon"]
+    database_path = arguments["<database_path>"]
+    passphrase = arguments["<passphrase>"]
+    host = arguments["--host"]
+    port = arguments["--port"]
+    assert port.isdigit()
+
+    # backend
+    backend_class = backends.get_backend_by_file(database_path)
+    backend = backend_class(database_path, passphrase)
+
+    # server
+    server = KeepassHTTPServer((host, int(port)), KeepassHTTPRequestHandler)
+    server.set_is_daemon(is_daemon)
+    server.set_backend(backend)
+
+    log.debug("Use Keepass Backend: %s" % backend.__class__.__name__)
+    log.info("Server started on %s:%s" % (host, port))
+
+    if is_daemon:
+        context = daemon.DaemonContext(stdout=open("/tmp/o.log", "w"),
+                                       stderr=open("/tmp/e.log", "w"),
+                                       files_preserve=[server.fileno()],
+                                       pidfile=lockfile.FileLock('/tmp/spam.pid'),
+                                       )
+
+
+        with context:
+            setproctitle.setproctitle("Keepass HTTPD server on %s:%s" % (host, port))
+            server.serve_forever()
+
+    else:
+        server.serve_forever()
