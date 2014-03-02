c66906e20fd0bae9122b7d5c0f6a0891a659a98d
Benjamin Hedrich <kiwisauce@pagenotfound.de>
2014-03-02 17:45:47 +0100

    Some cosmetics.

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-import sys
-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("requirements/package.txt") as requirements_file:
     requirements = requirements_file.read().splitlines()
@@ -11,12 +9,15 @@ setup(name="keepass_http",
       author="Benjamin Hedrich",
       author_email="kiwisauce@pagenotfound.de",
       url="https://github.com/bhedrich/python-keepass-httpd/",
-      package_dir = {"": "src"},
-      packages = find_packages("src/", exclude="tests"),
+      package_dir={"": "src"},
+      packages=find_packages("src/", exclude="tests"),
       install_requires=requirements,
       entry_points={"console_scripts":
                         ["python-keepass-httpd = python_keepass_httpd:main"]
       },
       zip_safe=False,
-      #data_files=[('usr/share/config', ['bla.conf']),],
+      include_package_data=True,
+      package_data={"keepass_http":
+                        ['conf/*.conf']
+      },
 )
