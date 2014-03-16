c1c1fa067d064db177762c489293fa566bddd0b1
Benjamin Hedrich <kiwisauce@pagenotfound.de>
2014-03-16 13:56:39 +0100

    Bump version: 0.3 → 0.3.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst") as readme_file:
     long_description = readme_file.read()
 
 setup(name="keepass_http",
-      version="0.3",
+      version="0.3.5",
       description="Python Keepass HTTPD for ChromeIPass",
       long_description=long_description,
       author="Benjamin Hedrich",
