ced2d8dc698a9c719c37e9dbc48fd9aa2d78f87c
Benjamin Hedrich <kiwisauce@pagenotfound.de>
2014-04-12 20:40:43 +0200

    Refactor the base Request class and its subclasses
    Rework impractical code fragments. There was too much code for
    too less functionality.
    
    Remove "request_dict" headers from some methods (saving the request
    dictionary in the as request-attribute)
    
    Get the client id via extra method.

--- tests/backend/test_get_backend.py
+++ tests/backend/test_get_backend.py
@@ -3,9 +3,11 @@ import pytest
 
 from keepass_http.backends import (get_backend_by_file, libkeepass_backend, NoBackendError,
                                    python_keepass_backend)
+from keepass_http.core import Conf
 
 
 def test_get_backend_by_mimetype():
+    Conf()
     assert get_backend_by_file("foo.kdb") is python_keepass_backend.Backend
     assert get_backend_by_file("foo.kdbx") is libkeepass_backend.Backend
 
