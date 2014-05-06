a9c9b0a565977d61977bcbb6701a8fcf1459093f
Benjamin Hedrich <kiwisauce@pagenotfound.de>
2014-05-06 23:16:10 +0200

    Add GUI (QT) tests
    Those are skipped if PySide is not installed.

---
+++ tests/gui/test_require_passphrase.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+import mock
+import pytest
+
+from keepass_http.backends import WrongPassword
+from keepass_http.utils import has_gui_support
+
+if has_gui_support():
+    from keepass_http.ui import gui
+    from PySide import QtGui, QtCore
+
+
+class TestBackend(mock.Mock):
+    open_database = mock.Mock()
+
+
+class TestConf(mock.Mock):
+    backend = TestBackend()
+
+
+skip_if_has_no_gui_support = pytest.mark.skipif(not has_gui_support(), reason="No QT Support")
+
+
+@skip_if_has_no_gui_support
+def test_require_ui_passphrase_correct(qtbot):
+    with mock.patch("keepass_http.ui.gui.Conf", TestConf):
+        window = gui.RequireDatabasePassphraseUi()
+
+        window.ui.show()
+        qtbot.addWidget(window.ui)
+
+        qtbot.waitForWindowShown(window.ui)
+
+        # password input field
+        window.ui.passphrase = mock.Mock()
+        window.ui.passphrase.text.return_value = "some passphrase"
+
+        buttons = window.ui.buttons
+        button_ok = buttons.button(buttons.Ok)
+
+        with mock.patch.object(TestBackend, "open_database") as mock_open_database:
+            qtbot.mouseClick(button_ok, QtCore.Qt.LeftButton)
+
+            mock_open_database.assert_called_once_with(u'some passphrase')
+        assert window._success is True
+
+
+@skip_if_has_no_gui_support
+def test_require_ui_passphrase_incorrect(qtbot):
+    with mock.patch("keepass_http.ui.gui.Conf", TestConf):
+        window = gui.RequireDatabasePassphraseUi()
+
+        window.ui.show()
+        qtbot.addWidget(window.ui)
+
+        qtbot.waitForWindowShown(window.ui)
+
+        # password input field
+        window.ui.passphrase = mock.Mock()
+        window.ui.passphrase.text.return_value = "some passphrase"
+
+        buttons = window.ui.buttons
+        button_ok = buttons.button(buttons.Ok)
+
+        with mock.patch.object(TestBackend, "open_database") as mock_open_database:
+            # when calling open_database() than raise the WrongPassword exception
+            mock_open_database.side_effect = [WrongPassword()]
+
+            qtbot.mouseClick(button_ok, QtCore.Qt.LeftButton)
+
+            mock_open_database.assert_called_once_with(u'some passphrase')
+
+        assert window._success is None
+
+
+@skip_if_has_no_gui_support
+def test_require_ui_passphrase_quit_or_cancel(qtbot):
+    window = gui.RequireDatabasePassphraseUi()
+
+    window.ui.show()
+    qtbot.addWidget(window.ui)
+
+    qtbot.waitForWindowShown(window.ui)
+
+    buttons = window.ui.buttons
+    button_ok = buttons.button(buttons.Cancel)
+
+    qtbot.mouseClick(button_ok, QtCore.Qt.LeftButton)
+
+    assert window._success is False
